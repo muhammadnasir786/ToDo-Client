14f45eec8605562332f566ae92f387e52f7326e6
muhammadnasir786 <nasir03082409229@gmail.com>
2018-08-15 18:17:07 +0500

    add server File

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express  = require('express');
 const app = express();
-app.set(express.static('build/index.html'))
+// app.set(express.static('build/index.html'))
+app.get('/',(req,res)=>{
+   res.sendfile('./build/index.html')
+})
 app.listen(process.env.PORT || 8080 ,()=>{
     console.log('Connection ')
 })
